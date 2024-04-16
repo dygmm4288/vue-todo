924183b1f632d36d3017b5403c59fa36edf7745c
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-04-16 14:31:38 +0900

    todo list v-for 이용하여 랜더링 완료

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import TodoForm from "./components/TodoForm.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
   name: "App",
   setup() {},
   components: {
     TodoForm,
+    TodoList,
   },
 };
 </script>
@@ -14,5 +16,6 @@ export default {
   <section>
     <h1>What is your focus on today</h1>
     <TodoForm />
+    <TodoList />
   </section>
 </template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+defineComponent({ name: "TodoList" });
+
+const todos = [
+  { id: 1, description: "hello", isDone: false },
+  { id: 2, description: "world", isDone: true },
+  { id: 3, description: "vue", isDone: false },
+];
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todos">
+      <p>{{ todo.description }}</p>
+      <button>{{ todo.isDone ? "working" : "done" }}</button>
+      <button>remove</button>
+    </li>
+  </ul>
+</template>
