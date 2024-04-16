694db59d2d6bd31b2a39736a99d27e0131d3a5e0
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-04-16 14:37:38 +0900

    todo item prop 적용 및 todo list에 연결

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { Todo } from "../types/todo.type";
+
+const { todo } = defineProps<{ todo: Todo }>();
+</script>
+
+<template>
+  <li>
+    <p>{{ todo.description }}</p>
+    <button>{{ todo.isDone ? "working" : "done" }}</button>
+    <button>remove</button>
+  </li>
+</template>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
 import { defineComponent } from "vue";
-defineComponent({ name: "TodoList" });
+import { Todo } from "../types/todo.type";
+import TodoItem from "./TodoItem.vue";
+defineComponent({ name: "TodoList", components: { TodoItem } });
 
 const todos = [
   { id: 1, description: "hello", isDone: false },
   { id: 2, description: "world", isDone: true },
   { id: 3, description: "vue", isDone: false },
-];
+] as Todo[];
 </script>
 
 <template>
   <ul>
-    <li v-for="todo in todos">
-      <p>{{ todo.description }}</p>
-      <button>{{ todo.isDone ? "working" : "done" }}</button>
-      <button>remove</button>
-    </li>
+    <TodoItem v-for="todo in todos" :todo="todo" />
   </ul>
 </template>
